05003af49b40f37ffa8157253009d5f879a47005
Akhilesh Bhatia <akhileshbhatia93@gmail.com>
2019-07-04 15:01:10 +0100

    svm with tf idf

--- SvmSentimentalAnalysis.py
+++ SvmSentimentalAnalysis.py
@@ -7,7 +7,7 @@
 import pandas as pd
 import re
 import html
-from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.svm import LinearSVC
 from sklearn.metrics import accuracy_score
 from nltk.corpus import stopwords
@@ -57,10 +57,10 @@ reviews_test = df_test["review"]
 reviews_test_clean = preprocess_reviews(reviews_test)
 reviews_test_clean = lemmatize_reviews(reviews_test_clean)
 
-wc_vectorizer = CountVectorizer(binary=False,ngram_range=(1,2))
-wc_vectorizer.fit(reviews_train_clean)
-training_data = wc_vectorizer.transform(reviews_train_clean)
-test_data = wc_vectorizer.transform(reviews_test_clean)
+tfidf_vectorizer = TfidfVectorizer(ngram_range=(1,2))
+tfidf_vectorizer.fit(reviews_train_clean)
+training_data = tfidf_vectorizer.transform(reviews_train_clean)
+test_data = tfidf_vectorizer.transform(reviews_test_clean)
 
 training_data_output = [1 if rating > 5 else 0 for rating in df_train["rating"]]
 
@@ -73,7 +73,7 @@ print("Accuracy is ",accuracy_score(test_data_expected_output, predictions))
 
 feature_to_coef = {
     word: coef 
-    for word, coef in zip(wc_vectorizer.get_feature_names(),svm.coef_[0])
+    for word, coef in zip(tfidf_vectorizer.get_feature_names(),svm.coef_[0])
     }
  
 print("Positive: ")
